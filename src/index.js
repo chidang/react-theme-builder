873be63769dde40b6d3bb5bcbdac00d1c17bc960
Chi Dang <dangtanminhchi@gmail.com>
2021-01-08 21:51:01 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import * as themes from './theme/schema.json';
+import { setToLS } from './utils/storage';
+
+setToLS('all-themes', themes.default);
 
 ReactDOM.render(
   <React.StrictMode>
